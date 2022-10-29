b68dbd3a6fa0ffeb31c59d1025c01a862b30fa56
JarJarSyncs <jettcurrie@outlook.com>
2022-10-30 01:20:03 +1100

    format test data

---
+++ formatTestBundooraDataset.py
@@ -0,0 +1,21 @@
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils.cell import get_column_letter
+from datetime import datetime
+
+newWorkbook = Workbook()
+workbook = load_workbook(filename="C:/Users/jettc/OneDrive - Swinburne University/3rd Year/Semester 2/Intelligent systems/Assignment A3/rawTest.xlsx", data_only=True)
+
+sheet = workbook.active
+newSheet = newWorkbook.active
+
+for cell in sheet['C']:
+    if cell.row > 2:
+
+        for timeCell in (sheet["E"+str(cell.row)+":"+"CV"+str(cell.row)])[0]:
+            row = newSheet.max_row + 1
+            newSheet["A" + str(row)] = cell.value.replace(hour=0,minute=0).strftime("%d/%m/%Y") + " " + str(sheet[get_column_letter(timeCell.column)+"1"].value)
+            newSheet["B"+ str(row)] = timeCell.value
+            newSheet["C"+str(row)] = sheet["A"+str(cell.row)].value
+        print(cell.row)
+
+newWorkbook.save("C:/Users/jettc/OneDrive - Swinburne University/3rd Year/Semester 2/Intelligent systems/Assignment A3/filteredTestData.xlsx")
